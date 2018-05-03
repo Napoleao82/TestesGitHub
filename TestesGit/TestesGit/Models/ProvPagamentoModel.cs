f15b4e05a2be4226620db1f5e8b7120d453cf4a3
Napoleão Menezes <napoleao82@gmail.com>
2018-05-03 15:13:01 -0300

    Issue1Com6

--- TestesGit/TestesGit/Models/ProvPagamentoModel.cs
+++ TestesGit/TestesGit/Models/ProvPagamentoModel.cs
@@ -15,5 +15,6 @@ namespace TestesGit.Models
         public string Issue1Com3 { get; set; }
         public string Issue1Com4 { get; set; }
         public string Issue1Com5 { get; set; }
+        public string Issue1Com6 { get; set; }
     }
 }

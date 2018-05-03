957eca8964023cee130e19c8926a3d1e0d01dafc
Napoleão Menezes <napoleao82@gmail.com>
2018-05-03 10:21:53 -0300

    Com 2

---
+++ TestesGit/TestesGit/Models/ProvPagamentoModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestesGit.Models
+{
+    public class ProvPagamentoModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Data { get; set; }
+        public decimal Valor { get; set; }
+    }
+}
